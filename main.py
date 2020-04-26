8f2540b8d89acf999bfca6f45b62f7a9a947afdf
AugustinJose1221 <augustinjose1221@gmail.com>
2020-04-26 14:41:26 +0530

    Updated prompt

--- main.py
+++ main.py
@@ -10,18 +10,55 @@ import os
 import platform
 import subprocess
 import sys
+
+from cmd import Cmd
 from pyfiglet import Figlet
+
 if platform.system() == "Windows":
     import msvcrt
     
-
-f = Figlet(font='slant')
-print(f.renderText('Silver - A Python shell prompt'))
+try:
+    from termcolor import colored
     
+    
+    
+except ImportError:
+    colored = None
+
+class silver(Cmd):
+    f = Figlet(font='slant')
+    print(f.renderText('Silver - A Python shell prompt'))
+    intro = colored("\n                                                                \n                   🏇 Hi-Yo, Silver! Away!                      \n                                                                ", "grey", "on_white") + "\n"
+    def updatePrompt():
+        if platform.system() == "Windows":
+            prompt = colored(str("~") + str(os.getcwd()) + str("> "), "green")
+        else:
+            prompt = colored(str("~") + str(os.getcwd()) + str("> "), "green", attrs=['bold'])
+        
+        
+    def do_exit(self, inp):
+        '''exit the application. Shorthand: x q.'''
+        print("Bye")
+        return True
+ 
+    def default(self, inp):
+        if inp == 'x' or inp == 'q':
+            return self.do_exit(inp)
+ 
+        print("Default: {}".format(inp))
+ 
+
+if __name__ == '__main__':  
+     silver().cmdloop()
+'''
 print(os.name)
 print(os.getcwd())
-'''
+
 os.chdir("/home/bernd/dropbox/websites/python-course.eu/cities")
 print(os.getcwd())
+
+print(os.listdir()) 
+string = "Test"
+color = "green"
+print(colored(string, color))
 '''
-print(os.listdir())
