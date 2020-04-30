5b2ff1af472244dfdd00829c35a972beef6b9d65
AugustinJose1221 <augustinjose1221@gmail.com>
2020-04-30 12:41:52 +0530

    Added Project Description

--- main.py
+++ main.py
@@ -32,17 +32,20 @@ Created on Sun Apr 26 11:04:48 2020
                                                                 
 
 
+Silver is a customizable shell written in Python. Silver can run basic linux and windows commands. The source code of
+silver is available at "https://github.com/AugustinJose1221/silver". Making custom commands and configuring commands 
+do multiple tasks is possible by making required tweaks to the source code. The entire code is written in Python and 
+compatible with Windows and Unix systems. Open issues to raise errors found while installation or runtime. Any 
+additional features/commands can be added by making pull requests to the master git branch.
 
+Description
+-----------
 
-
-
-
-
-
-
-
-
-
+Release version            1.0
+Author                     Augustin Jose
+Python version             3.6
+Supported Platforms        Windows and UNIX
+    
 
 
 
